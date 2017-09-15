176ed55f685174126ae8d660e51699cfbfe31f07
Phil Peng <phil.peng@sap.com>
2017-09-16 01:06:11 +0800

    phase2

--- test.js
+++ test.js
@@ -5,7 +5,8 @@ var roomNumber = core.createRoom(9);
 var step;
 
 var witchSeatNumber;
-var seerSeatNumber;
+var guardSeatNumber;
+var hunterSeatNumber;
 
 core.sitDown(roomNumber, 1);
 core.sitDown(roomNumber, 2);
@@ -26,28 +27,53 @@ for (var i = 0; i < 9; i++) {
     if (role.role === "witch") {
         witchSeatNumber = role.seatNumber;
     }
+    if (role.role === "guard") {
+        guardSeatNumber = role.seatNumber;
+    }
+    if (role.role === "hunter") {
+        hunterSeatNumber = role.seatNumber;
+    }
 }
 
 var gameStart = core.gameStart(roomNumber);
 
 console.log(gameStart);
 
-console.log("===wolf===");
+console.log("===狼人===");
 var getAlives = core.getAlives(roomNumber, "wolf");
 step = getAlives.step;
-console.log("Alive: ");
+console.log("狼人请刀人: ");
 console.log(getAlives);
-console.log("kill "+witchSeatNumber);
-core.wolfKill(roomNumber, step, witchSeatNumber);
-console.log("==witch===");
+console.log("狼刀: " + hunterSeatNumber);
+core.wolfKill(roomNumber, step, hunterSeatNumber);
+console.log("==女巫===");
 var lastKill = core.getKilledWitch(roomNumber, witchSeatNumber);
-console.log("Last killed: " + lastKill);
+console.log("这个人死了，你要救吗: " + lastKill);
 if (lastKill > 0) {
-    console.log("heal");
+    console.log("救");
     core.healWitch(roomNumber, witchSeatNumber, lastKill);
 }
 
-console.log("===seer===");
-var getAlives2 = core.getAlives(roomNumber);
-console.log("Alive2: ");
-console.log(getAlives2);
+console.log("===预言家===");
+var getAlivesSeer = core.getAlives(roomNumber);
+console.log("预言家你要验谁: ");
+console.log(getAlivesSeer);
+console.log("验：" + witchSeatNumber);
+var seerResult = core.getRoleSeer(roomNumber, witchSeatNumber);
+console.log(seerResult);
+
+console.log("===守卫===");
+var getAlivesGuard = core.getAlivesGuard(roomNumber, guardSeatNumber);
+console.log("守卫你要守谁: ");
+console.log(getAlivesGuard);
+core.actionGuard(roomNumber, hunterSeatNumber);
+console.log("守: " + hunterSeatNumber);
+core.calculateResult(roomNumber);
+
+console.log("===猎人===");
+var actionHunter = core.getActionHunter(roomNumber, hunterSeatNumber);
+console.log(actionHunter);
+
+console.log("===结算===");
+var finalResult = core.getDead(roomNumber);
+console.log(finalResult);
